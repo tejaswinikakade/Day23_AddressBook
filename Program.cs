4e8f1f23c96746a22545d407f0c0b62da89e2df2
tejaswinikakade <tejukakade1821@gmail.com>
2021-11-19 23:27:54 +0530

    Adding UC1 Create a Contacts in Address

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AddressBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AddressBooks.AddrBook.GetCustomer();
+            AddressBooks.AddrBook.ListingPeople();
+
+        }
+    }
+}
